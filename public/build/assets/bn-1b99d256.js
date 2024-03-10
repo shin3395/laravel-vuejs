e47c32f2df112d55e2789d73f1debb42d2d37cae
nhi <dongnhi3395n@gmail.com>
2024-03-10 17:53:47 +0700

    init - demo

---
+++ public/build/assets/bn-1b99d256.js
@@ -0,0 +1,1 @@
+const e="Laravel Vue 3 Starter-এ স্বাগতম",t="Ok",c="Cancel",a={welcome_starter_title:e,ok:t,cancel:c};export{c as cancel,a as default,t as ok,e as welcome_starter_title};
---
+++ public/build/assets/index-66cc6d5f.js
@@ -0,0 +1,1 @@
+import{_ as e,o as n,b as c}from"./app-25ba0aa8.js";const o={};function r(t,s){return n(),c("h1",null,"Admin")}const a=e(o,[["render",r]]);export{a as default};
